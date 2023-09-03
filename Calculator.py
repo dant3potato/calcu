4a4b220a2fb93fab1ddaf9d327fb960f50e69696
dant3potato <142431828+dant3potato@users.noreply.github.com>
2023-09-03 21:14:33 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -8,15 +8,14 @@ root.maxsize(width=420, height=420)
 root.configure(bg="gray")
 
 
-value = StringVar()  # it is a string variable
-value.set("")  # here we set is value blank
+value = StringVar() 
+value.set("") 
 ent = Entry(root, text=value, width=25, borderwidth=12, border=7, font=('24'))
 ent.pack(padx=10, pady=1)
 
 
-def click(event):  # bind function takes by default event positional argument
-    global value  # value become global
-    # Gives the button by event.widget and cget gives the text from the button
+def click(event): 
+    global value 
     text = event.widget.cget("text")
     # print(text)
     if text == "=":
@@ -39,58 +38,57 @@ canvas.pack()
 # Buttons
 B1 = Button(canvas, text="1", padx=20, pady=20)
 B1.grid(row=3, column=2)
-B1.bind("<Button>", click)  # here binding the button with click fuction
+B1.bind("<Button>", click) 
 B2 = Button(canvas, text="2", padx=20, pady=20)
 B2.grid(row=3, column=1)
-B2.bind("<Button>", click)  # here binding the button with click fuction
+B2.bind("<Button>", click)
 B3 = Button(canvas, text="3", padx=20, pady=20)
 B3.grid(row=3, column=0, pady=1)
-B3.bind("<Button>", click)  # here binding the button with click fuction
+B3.bind("<Button>", click)  
 B4 = Button(canvas, text="4", padx=20, pady=20)
 B4.grid(row=2, column=2)
-B4.bind("<Button>", click)  # here binding the button with click fuction
+B4.bind("<Button>", click) 
 B5 = Button(canvas, text="5", padx=20, pady=20)
 B5.grid(row=2, column=1)
-B5.bind("<Button>", click)  # here binding the button with click fuction
+B5.bind("<Button>", click) 
 B6 = Button(canvas, text="6", padx=20, pady=20)
 B6.grid(row=2, column=0, pady=1)
-B6.bind("<Button>", click)  # here binding the button with click fuction
+B6.bind("<Button>", click) 
 B7 = Button(canvas, text="7", padx=20, pady=20)
 B7.grid(row=1, column=2)
-B7.bind("<Button>", click)  # here binding the button with click fuction
+B7.bind("<Button>", click)  
 B8 = Button(canvas, text="8", padx=20, pady=20)
 B8.grid(row=1, column=1)
-B8.bind("<Button>", click)  # here binding the button with click fuction
+B8.bind("<Button>", click)  
 B9 = Button(canvas, text="9", padx=20, pady=20)
 B9.grid(row=1, column=0, pady=1)
-B9.bind("<Button>", click)  # here binding the button with click fuction
+B9.bind("<Button>", click)  
 zero = Button(canvas, text="0", padx=20, pady=20)
 zero.grid(row=4, column=0, pady=1)
-# here binding the  Zero button with click fuction
 zero.bind("<Button>", click)
 add = Button(canvas, text="+", padx=19, pady=20)
 add.grid(row=1, column=3)
-add.bind("<Button>", click)  # here binding the button with click fuction
+add.bind("<Button>", click)  
 sub = Button(canvas, text="-", padx=20, pady=20)
 sub.grid(row=2, column=3)
-sub.bind("<Button>", click)  # here binding the button with click fuction
+sub.bind("<Button>", click)  
 mul = Button(canvas, text="*", padx=20, pady=20)
 mul.grid(row=3, column=3)
-mul.bind("<Button>", click)  # here binding the button with click fuction
+mul.bind("<Button>", click)
 div = Button(canvas, text="/", padx=20, pady=20)
 div.grid(row=4, column=3)
-div.bind("<Button>", click)  # here binding the button with click fuction
+div.bind("<Button>", click)  
 equal = Button(canvas, text="=", padx=55)
 equal.grid(row=0, columnspan=2, column=2, padx=4)
-equal.bind("<Button>", click)  # here binding the button with click fuction
+equal.bind("<Button>", click)  
 clear = Button(canvas, text="AC", padx=60)
 clear.grid(row=0, columnspan=2, column=0, padx=4, pady=5)
-clear.bind("<Button>", click)  # here binding the button with click fuction
+clear.bind("<Button>", click)  
 Double_Zero = Button(canvas, text="00", padx=20, pady=20)
 Double_Zero.grid(row=4, column=1)
 # here binding the button with click fuction
 Double_Zero.bind("<Button>", click)
 Dot = Button(canvas, text=".", padx=20, pady=20)
 Dot.grid(row=4, column=2)
-Dot.bind("<Button>", click)  # here binding the button with click fuction
+Dot.bind("<Button>", click)  
 root.mainloop()
